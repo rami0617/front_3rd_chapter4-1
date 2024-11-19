f1f7f69801e706475321a3f088039c899da67866
최아람 <aarmakrr@hanmail.net>
2024-11-19 12:44:41 +0900

    [fix]: 비디오를 로컬에 넣음

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/qynlJNIb4SU?si=IdhHKr8iBWTpthlg"
+          src="/mangom.mp4"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
